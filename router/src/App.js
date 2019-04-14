1f68f45b686a998f89b4e231013c57bda6c66ebf
azerbelhedi <azerbelhedide@gmail.com>
2019-04-14 23:41:04 +0100

    react-router demo router route link switch

--- router/src/App.js
+++ router/src/App.js
@@ -1,27 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Neutron from './components/neutron' ;
+import { BrowserRouter as Router , Route , Link , Switch } from 'react-router-dom' ; 
+
+const NavBar = () =>{
+  return(
+    <div>
+      <ul>
+        <li>
+          <Link to = "/">Home</Link>
+        </li>
+        <li>
+          <Link to = "/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to = "/login">Login</Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+const Home = () => {
+  return(
+    <h1>Home</h1>
+  )
+}
+
+const Profile = () =>{
+  return(
+    <h1>Profile</h1>
+  )
+}
+
+const Login = () =>{
+  return(
+    <h1>Login</h1>
+  )
+}
+
+const Error = () =>{
+  return(
+    <h1>404 page not found</h1>
+  )
+}
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return(
+      <Router>
+        <div>
+        <NavBar/>
+        <Switch>
+          <Route  exact path = "/" component = {Home}/>
+          <Route path = "/profile" component = {Profile} />
+          <Route path = "/login" component = {Login} />
+          <Route component = {Error} />
+        </Switch>
+        </div>
+      </Router>
+    )
   }
 }
 
---
+++ router/src/components/neutron.js
@@ -0,0 +1,17 @@
+import React , { Component } from 'react' ;
+
+class Neutron extends Component{
+    constructor(props){
+        super(props) ;
+    }
+
+    render(){
+        return(
+            <div style = {{width : "100vw" , backgroundColor : "red"}}>
+                <h1>azer</h1>
+            </div>
+        )
+    }
+}
+
+export default Neutron ;
